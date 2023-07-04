0631d31f570750a976780866061ed89765b18983
Lawi-Mwaura <Lawimwaura@gmail.com>
2023-07-04 06:43:34 +0300

    Fixed poster displays

--- index.js
+++ index.js
@@ -1,48 +1,81 @@
-document.addEventListener('DOMContentLoaded',() =
-})
-
-
-//Fetch JSON data from url
-let url= ` http://localhost:3000/films`
-async function fetchData(url){
-    try{
+document.addEventListener('DOMContentLoaded', () => {
+    const url = 'http://localhost:3000/films';
+  
+    async function fetchData(url) {
+      try {
         const response = await fetch(url);
-        if(!response.ok){
-            throw new Error("Network response is faulty");
+        if (!response.ok) {
+          throw new Error('Network response is faulty');
         }
         return await response.json();
-    } catch (error){
+      } catch (error) {
         return null;
+      }
     }
-}
-
-// Fetch and show results based on url
-async function fetchAndShowResults(url){
-    const data = await fetchData(url)
-    if(data && data.results) {
-        showResults(data.results);
+  
+    async function fetchAndShowResults(url) {
+      const data = await fetchData(url);
+      if (data && data.length) {
+        showResults(data);
+      }
     }
-}
-
-//Posters for the movie 
-function displayMoviePosters(movie){
-    const {title, runtime, capacity, showtime, ticketsSold, Description, poster} = movie;
-
-}
-
-
-
-
-//Adding Event listeners
-form.addEventListener('submit', handleSearch);
-window.addEventListener('scroll', detected);
-window.addEventListener('resize',detectEnd);
-
-//Initialization of the page
-async function init(){
-    clearResults();
-    const url= `http://localhost:3000/films`;
-    isSearching = false;
-    await fetchAndShowResults(url);
-
-}
+  
+    function showResults(movies) {
+      const cardContainer = document.querySelector('.card-container');
+      cardContainer.innerHTML = '';
+  
+      const rowSize = 4; // Number of cards per row
+      let currentRow;
+  
+      movies.forEach((movie, index) => {
+        const { id, title, runtime, capacity, showtime, ticketsSold, Description, poster } = movie;
+  
+        if (index % rowSize === 0) {
+          currentRow = document.createElement('div');
+          currentRow.classList.add('row');
+          cardContainer.appendChild(currentRow);
+        }
+  
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+  
+        const cardContent = `
+          <h2 class="card-title">${title || ''}</h2>
+          <p class="card-runtime">Runtime: ${runtime || ''} minutes</p>
+          <p class="card-capacity">Capacity: ${capacity || ''}</p>
+          <p class="card-showtime">Showtime: ${showtime || ''}</p>
+          <p class="card-tickets-sold">Tickets Sold: ${ticketsSold || 0}</p>
+          <p class="card-description">${Description || ''}</p>
+          <div class="card-media">
+            <img src="${poster || ''}" alt="Movie Poster" width="100%">
+          </div>
+          <button class="buy-ticket-btn" data-movie-id="${id}">Buy Ticket</button>
+        `;
+  
+        cardElement.innerHTML = cardContent;
+        currentRow.appendChild(cardElement);
+      });
+  
+      addBuyTicketListeners();
+    }
+  
+    function addBuyTicketListeners() {
+      const buyTicketButtons = document.querySelectorAll('.buy-ticket-btn');
+      buyTicketButtons.forEach(button => {
+        button.addEventListener('click', handleBuyTicket);
+      });
+    }
+  
+    function handleBuyTicket(event) {
+      const movieId = event.target.dataset.movieId;
+      // Perform the necessary action to buy a ticket for the movie with the given movieId
+      console.log(`Buying ticket for movie with ID: ${movieId}`);
+    }
+  
+    async function init() {
+      await fetchAndShowResults(url);
+    }
+  
+    init();
+  });
+  
